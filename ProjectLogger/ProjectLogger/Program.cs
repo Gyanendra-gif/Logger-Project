a54be597bcbc1aad8f7bc386eb942089af1667cc
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-16 10:27:26 +0530

    [Gyanendra] Add: A Program of Logger File

---
+++ ProjectLogger/ProjectLogger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers ad = new AddNumbers();
+            ad.Sum(10, 5);
+        }
+    }
+}
